75184d5c94e25f05820833f503fa7aeb2925348e
berilo queiroz <luiz.berilo@buson.com.br>
2023-07-12 08:45:42 -0300

    descomentando e2e client

--- src/api/__tests__/client.e2e.spec.ts
+++ src/api/__tests__/client.e2e.spec.ts
@@ -11,31 +11,31 @@ describe("E2E test for client", () => {
   });
 
   it("should create a client", async () => {
-    // const response = await request(app)
-    //   .post("/clients")
-    //   .send({
-    //     id: "1",
-    //     name: "John",
-    //     email: "john@x.com",
-    //     document: "1236",
-    //     street: "rua dos bobos",
-    //     number: "0",
-    //     complement: "",
-    //     city: "doida",
-    //     state: "NA",
-    //     zipCode: "6000000"
-    //   });
+    const response = await request(app)
+      .post("/clients")
+      .send({
+        id: "1",
+        name: "John",
+        email: "john@x.com",
+        document: "1236",
+        street: "rua dos bobos",
+        number: "0",
+        complement: "",
+        city: "doida",
+        state: "NA",
+        zipCode: "6000000"
+      });
 
-    // expect(response.status).toBe(200);
-    // expect(response.body.name).toBe("John");
-    // expect(response.body.email).toBe("john@x.com");
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("John");
+    expect(response.body.email).toBe("john@x.com");
   });
 
   it("should not create a client", async () => {
-    // const response = await request(app).post("/clients").send({
-    //   name: "john",
-    // });
-    // expect(response.status).toBe(500);
+    const response = await request(app).post("/clients").send({
+      name: "john",
+    });
+    expect(response.status).toBe(500);
   });
 
 });
